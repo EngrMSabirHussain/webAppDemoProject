db3466b01c37c60967f7ef4526f0edb5c895fcbd
Sabir Hussain <58667012+EngrMSabirHussain@users.noreply.github.com>
2023-03-30 16:01:03 +0500

    Delete WebAppDemoProject directory

--- WebAppDemoProject/src/main/java/com/dropsourcing/repositiors/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dropsourcing.repositiors;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dropsourcing.entities.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
-
-}
--- WebAppDemoProject/src/main/java/com/dropsourcing/repositiors/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dropsourcing.repositiors;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dropsourcing.entities.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
