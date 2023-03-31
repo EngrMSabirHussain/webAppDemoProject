d344ff2c673d166fa0b80edfe104c8cd38222d88
Sabir Hussain <58667012+EngrMSabirHussain@users.noreply.github.com>
2023-03-31 16:09:06 +0500

    Delete WebAppDemoProject directory

--- WebAppDemoProject/src/main/java/com/dropsourcing/repositiors/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dropsourcing.repositiors;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dropsourcing.entities.Role;
-
-public interface RoleRepo extends JpaRepository<Role, Integer> {
-
-}
